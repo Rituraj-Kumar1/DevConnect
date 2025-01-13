d6fbcc245f5769f34b508cfae012e1014ccfa50c
Rituraj-Kumar1 <kumar.rituraj181@gmail.com>
2025-01-13 23:40:24 +0530

    final

--- src/app.js
+++ src/app.js
@@ -3,12 +3,15 @@ const app = express();
 // we can use multiple handlers for the same route
 app.use('/user', (req, res, next) => {
     console.log("first handler");
+    next();
+    console.log("after")
     res.send('responed by first handler');
-    // next();
-}, (req, res) => {
+}, (req, res, next) => {
     console.log("second handler");
     res.send('responed by second handler');
-})
+    // next();
+}
+)
 app.listen(7777, () => {
     console.log('Server is running on port  http://localhost:7777');
 });
