d7d874a0b655e9fec3dabb69e4f3b5f8e9516c95
Rituraj-Kumar1 <kumar.rituraj181@gmail.com>
2025-01-16 22:01:30 +0530

    lecture 6 mongo

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const MONGOURI = require('/utils/mongodetails')
 const dbconnect = async () => { //mongoose.connect returns promise
-    await mongoose.connect("mongodb+srv://niteshpariharprojects:KERv7Qr6GCMaXkoK@connectdevelopers.ij0qf.mongodb.net/ConnectDev")
+    await mongoose.connect(MONGOURI)
     // if we write anything after connect url like /ConnectDev then it will create database and refer to it
 };
 module.exports = {
